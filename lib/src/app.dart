c6348fa4f26dda4c41a016296c4da22bd5cb1441
Flutter Intern <flutter1.tork@gmail.com>
2024-03-16 15:47:20 +0600

    device and http helper added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:torganic/src/utils/device/device_utility.dart';
+import 'package:url_launcher/url_launcher.dart';
 import './utils/theme/theme.dart';
 import './utils/constants/text_strings.dart';
 
@@ -16,14 +18,16 @@ class MyApp extends StatelessWidget {
       darkTheme: MyAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       // initialBinding: GeneralBindings(),
-      home: const Scaffold(
+      home:  Scaffold(
         body: Center(
-          child: Text(
-            'Project Structure is set up and running.',
-            textAlign: TextAlign.center,
-          ),
+          child: ElevatedButton(
+            onPressed: (){},
+            child: Text('tap this'),
+          )
         ),
       ),
     );
   }
 }
+
+
