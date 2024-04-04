f4b4c59c1dccf60839e39e80db5689b1bee0cb22
Flutter Intern <flutter1.tork@gmail.com>
2024-04-04 11:51:21 +0600

    stripe added succesfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:torganic/src/app.dart';
@@ -17,8 +18,11 @@ Future<void> main() async {
   ).then((FirebaseApp value) => Get.put(AuthRepositories));
 
   await FirebaseApi().initNotifications();
+  Stripe.publishableKey =
+      "pk_test_51P1ip3P2j285qpY0QWFYsQJAG2AV51vaK43X3gFiAfWthlEsALRJS33aVZDZNOaS1hgMlBd20AG7ejNWqcPqvpW500bYRkMhG6";
 
-  SystemChrome.setSystemUIOverlayStyle( const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.dark));
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark));
   runApp(const MyApp());
 }
